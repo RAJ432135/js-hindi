2295f48fabc6a085b27ab57eaefacc272e35c217
CBUM <118042875+RAJ432135@users.noreply.github.com>
2025-06-03 09:23:50 +0000

    datatypes done

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+const accountId=5432
+let AccountEmail="shivamrjp4321@gmail.com"
+var AccountPassword="1234"
+accountcity="jaipur"
+let accountState;
+/*
+let is block] scope it is reassizn in block but not redecleared in block;
+var is global scope it is reassizn or redeclered 
+const is block scope it is redecleared ;
+those variable decleared not assigned give undefined
+
+*/
+console.table([accountId,AccountEmail,AccountPassword,accountState,accountcity])
+// console.tabel is used for dispaly the data in tabel format
+console.log() // it is used to print a message
