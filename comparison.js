bbe4992ad1d241a3939b9a82a4564c0efbe57d4d
CBUM <118042875+RAJ432135@users.noreply.github.com>
2025-06-03 11:16:40 +0000

    comparison

---
+++ comparison.js
@@ -0,0 +1,21 @@
+// comparision of datatype IN JS
+console.log(4>3)
+console.log(4<3)
+console.log(4>=3)
+console.log(4<=3)
+console.log(4==3)
+console.log(4!=3)
+
+// comparison with null and zero
+console.log(null>0) // it return false
+console.log(null<0) // it return false
+console.log(null==0) // it return false
+console.log(null>=0) // it return true  comparison convert null to number 0
+
+
+// undefined comparision always false with zero
+
+// comparison between === or ==
+console.log("2"==2) // it return true beacause it check only value not datatype
+
+console.log("2"===2)  // it return false because it check both value and datatype
