662a0500b62c7baf52245f3a8f4d4dab3b3be9ed
CBUM <118042875+RAJ432135@users.noreply.github.com>
2025-06-03 10:33:39 +0000

    string number

---
+++ stringisnumber.js
@@ -0,0 +1,35 @@
+// operation
+let value=3
+let negvalue=-value
+console.log(negvalue)
+
+/*
+console.log(3+3)
+console.log(4-3)
+console.log(3*3)
+console.log(10/2)
+console.log(2 **2)  duble star for 2 power 2
+console.log(2%13)
+*/
+
+let str1="shivam"
+let str2="kumar"
+let str3=str1+str2
+console.log(str3)
+
+console.log(1 +2 +" 2")
+console.log("1"+"2"+1)
+
+/*
+
+operation on the basis of first datatype
+
+*/
+let a=+true;
+console.log(a)   // it is used to conversion    + is used to conversion
+
+OPERATOR 
+let po=5;
+  // it is used to first increment then print
+console.log(++op)
+
