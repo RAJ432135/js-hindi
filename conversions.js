f328ac3b703520238f091a960f453b86e2155247
CBUM <118042875+RAJ432135@users.noreply.github.com>
2025-06-03 09:26:56 +0000

    conversion done

---
+++ conversions.js
@@ -0,0 +1,39 @@
+
+let number="33";
+let number1=Number(33);
+console.log(typeof(number1));
+let name="123er";
+console.log(typeof(name))
+console.log(name)
+let name2=Number(name);
+console.log(typeof(name2))
+console.log(name2)
+
+// string if "33" in string aftern conversion in number datatype show number and value 33
+/*
+string if "33adv" after conversion to number datatype show number but value show NaN
+
+IN BOOLEAN
+
+after conversion 1 to boolean then it show true for 0 false for empty value false for some value true
+*/
+
+let islogin=1;
+let islogin1=Boolean(islogin);
+console.log(islogin1);
+
+let sinup=0;
+let signup1=Boolean(sinup)
+console.log(signup1)
+
+let other="";
+let other1=Boolean(other);
+console.log(other1);
+let something="abn";
+let something1=Boolean(something);
+console.log(something1);
+
+let nb=1234
+let nb1=String(nb)
+console.log(nb1);
+console.log(typeof(nb1))
